35ab5774425a6536b375f8ed20c59b11cfe63d48
Bioninja01 <vinsonjames02@gmail.com>
2015-02-16 18:41:52 -0500

    new packages

---
+++ project/core/src/com/mygdx/entities/Enemy.java
@@ -0,0 +1,47 @@
+package com.mygdx.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.triggers.WayPoint;
+
+import java.util.LinkedList;
+
+/**
+ * Created by James on 2/14/2015.
+ */
+public class Enemy {
+    public Sprite sprite;
+    float x=0;
+    float y=0;
+    float velocity = 0;
+    LinkedList<WayPoint> wayPoints;
+
+
+    public Enemy(Texture img){
+        this.sprite = new Sprite(img);
+        sprite.setPosition(x,y);
+    }
+    public Enemy(Texture img, float x, float y, float velocity){
+        this.sprite = new Sprite(img);
+        this.x = x;
+        this.y = y;
+        this.velocity = velocity;
+        sprite.setPosition(x,y);
+        wayPoints = new LinkedList<WayPoint>();
+    }
+
+    public void Move(){sprite.setPosition(sprite.getX()+velocity,sprite.getY());}
+    public void Check(){
+        if(sprite.getX() == wayPoints.getFirst().x && sprite.getY() == wayPoints.getFirst().y){
+           velocity = -velocity;
+        }
+    }
+    public void SetWayPoint(float x, float y){wayPoints.add(new WayPoint(x,y));}
+
+
+    public void render(SpriteBatch sb){
+        sprite.draw(sb);
+    }
+
+}
---
+++ project/core/src/com/mygdx/triggers/WayPoint.java
@@ -0,0 +1,15 @@
+package com.mygdx.triggers;
+
+/**
+ * Created by James on 2/14/2015.
+ */
+public class WayPoint {
+    public float x;
+    public float y;
+
+    public  WayPoint(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+}
