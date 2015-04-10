57d9e190451616c4b924ab3ac9c16e0e86fc36c3
FosterTa <xxhedgemonxx@knights.ucf.edu>
2015-04-10 11:05:16 -0400

    Made NetTest look pretty.

--- project/core/src/com/mygdx/states/NetTest.java
+++ project/core/src/com/mygdx/states/NetTest.java
@@ -2,7 +2,9 @@ package com.mygdx.states;
 
 // LibGDX includes
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.mygdx.handlers.EnemyManager;
 import com.mygdx.handlers.GameStateManager;
 import com.mygdx.UI.MyStage;
 import com.badlogic.gdx.Gdx;
@@ -14,6 +16,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.mygdx.game.MyGame;
 import com.mygdx.handlers.NetworkManager;
+import com.mygdx.handlers.WayPointManager;
 import com.mygdx.handlers.action.Action;
 import com.mygdx.net.ConnectionMode;
 
@@ -27,6 +30,9 @@ public class NetTest extends GameState
     private TextButton serverButton;
     private TextButton clientButton;
     private OrthographicCamera cam;
+    private Texture Map = new Texture("Maps/SubMenuMap.png");
+    private WayPointManager wayPointManager;
+    private EnemyManager enemyManager;
 
     public NetTest(GameStateManager gameStateManager, final NetworkManager networkManager)
     {
@@ -37,15 +43,25 @@ public class NetTest extends GameState
         cam.setToOrtho(false, MyGame.V_WIDTH, MyGame.V_HEIGHT);
         Gdx.input.setInputProcessor(stage);
         Skin skin = new Skin(Gdx.files.internal("UiData/uiskin.json"));
+        com.mygdx.entities.Actor map = new com.mygdx.entities.Actor(Map, 0, 0);
+        stage.addActor(map);
+        wayPointManager = new WayPointManager(5);
+        enemyManager = new EnemyManager(networkManager,wayPointManager.wayPoints, stage.getBatch());
+        enemyManager.LevelSelectWave();
+        stage.addActor(enemyManager);
+
+
 
         serverButton = new TextButton("Server", skin);
-        serverButton.setPosition(MyGame.V_WIDTH / 4, MyGame.V_HEIGHT * 5 / 8);
+        serverButton.setSize(200, 60);
+        serverButton.setPosition(game.V_WIDTH/2-serverButton.getWidth()/2, MyGame.V_HEIGHT * 6/11);
         serverButton.addListener( new ClickListener());
 
         stage.addActor(serverButton);
 
         clientButton = new TextButton("Client", skin);
-        clientButton.setPosition(MyGame.V_WIDTH / 4, MyGame.V_HEIGHT / 4);
+        clientButton.setSize(200, 60);
+        clientButton.setPosition(game.V_WIDTH/2-serverButton.getWidth()/2, serverButton.getY() - 65);
         clientButton.addListener( new ClickListener());
         stage.addActor(clientButton);
     }
